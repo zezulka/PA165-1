070e9aab6d5100050bb6a5cc082a7b96a40105a0
Martin Kuba <makub@ics.muni.cz>
2015-12-04 13:34:09 +0100

    seminar-javascript step 2

--- eshop-angular/src/main/webapp/angular_app.js
+++ eshop-angular/src/main/webapp/angular_app.js
@@ -11,8 +11,34 @@ pa165eshopApp.config(['$routeProvider',
         $routeProvider.
         when('/shopping', {templateUrl: 'partials/shopping.html', controller: 'ShoppingCtrl'}).
         when('/product/:productId', {templateUrl: 'partials/product_detail.html', controller: 'ProductDetailCtrl'}).
+        when('/category/:categoryId', {templateUrl: 'partials/category_detail.html', controller: 'CategoryDetailCtrl'}).
+        when('/admin/products', {templateUrl: 'partials/admin_products.html', controller: 'AdminProductsCtrl'}).
+        when('/admin/newproduct', {templateUrl: 'partials/admin_new_product.html', controller: 'AdminNewProductCtrl'}).
         otherwise({redirectTo: '/shopping'});
     }]);
+
+/*
+ * alert closing functions defined in root scope to be available in every template
+ */
+pa165eshopApp.run(function($rootScope) {
+    $rootScope.hideSuccessAlert = function () {
+        $rootScope.successAlert = undefined;
+    };
+    $rootScope.hideWarningAlert = function () {
+        $rootScope.warningAlert = undefined;
+    };
+    $rootScope.hideErrorAlert = function () {
+        $rootScope.errorAlert = undefined;
+    };
+});
+
+
+
+
+
+/* Controllers */
+
+
 
 /*
  * Public eshop interface
@@ -43,6 +69,7 @@ eshopControllers.controller('ShoppingCtrl', function ($scope, $http) {
     });
 });
 
+
 /*
  * Product detail page
  */
@@ -55,3 +82,113 @@ eshopControllers.controller('ProductDetailCtrl',
             console.log('AJAX loaded detail of product ' + $scope.product.name);
         });
     });
+
+/*
+ * Category detail page
+ */
+
+eshopControllers.controller('CategoryDetailCtrl', ['$scope', '$routeParams', '$http',
+    function ($scope, $routeParams, $http) {
+        var categoryId = $routeParams.categoryId;
+        $http.get('/eshop/api/v1/categories/' + categoryId).then(function (response) {
+            var category = response.data;
+            $scope.category = category;
+            console.log('AJAX loaded detail of category ' + category.name);
+            loadCategoryProducts($http, category, category['_links'].products.href);
+        });
+    }]);
+
+/*
+ * Administration interface
+ */
+
+/*
+ * Admin Products page
+ */
+function loadAdminProducts($http, $scope) {
+    $http.get('/eshop/api/v1/products').then(function (response) {
+        $scope.products = response.data._embedded.products;
+        console.log('AJAX loaded all products ');
+    });
+}
+eshopControllers.controller('AdminProductsCtrl',
+    function ($scope, $rootScope, $routeParams, $http) {
+        //initial load of all products
+        loadAdminProducts($http, $scope);
+        // function called when Delete button is clicked
+        $scope.deleteProduct = function (product) {
+	        console.log("deleting product with id=" + product.id + ' (' + product.name + ')');
+            $http.delete(product._links.delete.href).then(
+                function success(response) {
+                    console.log('deleted product ' + product.id + ' on server');
+                    //display confirmation alert
+                    $rootScope.successAlert='Deleted product "'+product.name+'"';
+                    //load new list of all products
+                    loadAdminProducts($http, $scope);
+                },
+                function error(response) {
+                    console.log('server returned error');
+                    $rootScope.errorAlert = 'Cannot delete product "'+product.name+'"! It is used in an order.';
+                }
+            );
+        };
+    });
+
+
+/*
+ * Page with form for creating new product
+ */
+eshopControllers.controller('AdminNewProductCtrl',
+    function ($scope, $routeParams, $http, $location, $rootScope) {
+        //prepare data for selection lists
+        $scope.colors = ['RED', 'GREEN', 'BLUE', 'BLACK'];
+        $scope.currencies = ['CZK', 'EUR', 'USD'];
+        //get categories from server
+        $http.get('/eshop/api/v1/categories/').then(function (response) {
+            $scope.categories = response.data['_embedded']['categories'];
+        });
+        //set object bound to form fields
+        $scope.product = {
+            'name': '',
+            'description': '',
+            'categoryId': 1,
+            'price': 0,
+            'color': $scope.colors[1],
+            'currency': $scope.currencies[0]
+        };
+        // function called when submit button is clicked, creates product on server
+        $scope.create = function (product) {
+            $http({
+                method: 'POST',
+                url: '/eshop/api/v1/products/create',
+                data: product
+            }).then(function success(response) {
+                console.log('created product');
+                var createdProduct= response.data;
+                //display confirmation alert
+                $rootScope.successAlert = 'A new product "'+createdProduct.name+'" was created';
+                //change view to list of products
+                $location.path("/admin/products");
+            }, function error(response) {
+                //display error
+                $scope.errorAlert = 'Cannot create product !';
+            });
+        };
+    });
+
+// defines new directive (HTML attribute "convert-to-int") for conversion between string and int
+// of the value of a selection list in a form
+// without this, the value of the selected option is always a string, not an integer
+eshopControllers.directive('convertToInt', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attrs, ngModel) {
+            ngModel.$parsers.push(function (val) {
+                return parseInt(val, 10);
+            });
+            ngModel.$formatters.push(function (val) {
+                return '' + val;
+            });
+        }
+    };
+});
